028b6ca492aa8b65575fbb6b5fdcb3eca92aea66
dilrajkadavithara <dilrajkadavithara@gmail.com>
2026-03-03 14:17:21 +0530

    feat: Increase Polling Station Name length to 1000 characters to support long Malayalam names

--- voter_vault/core_db/models.py
+++ voter_vault/core_db/models.py
@@ -41,7 +41,7 @@ class Booth(models.Model):
     number = models.CharField(max_length=50, help_text="Booth Number (e.g. 001, 145A)")
     polling_station_no = models.CharField(max_length=100, blank=True, help_text="Polling Station Number (if different from booth number)")
     name = models.CharField(max_length=300, blank=True, help_text="Optional name/location of the booth")
-    polling_station_name = models.CharField(max_length=500, blank=True, help_text="Physical location of the polling station")
+    polling_station_name = models.CharField(max_length=1000, blank=True, help_text="Physical location of the polling station")
     head_name = models.CharField(max_length=200, blank=True, null=True, help_text="Primary contact person for this booth")
     head_phone = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
